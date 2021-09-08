052d4544b9a6e32260640a7c163c99d84e5f5d30
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2021-09-08 19:49:44 -0300

    Initialize getx

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GetX Example Social',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
