fb803a2d26675e7df8d27075ba90bdb423e578cb
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2021-09-10 15:13:49 -0300

    Loading widget

---
+++ lib/presentation/widgets/loading_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      height: size.height,
+      width: size.width,
+      color: Colors.white70,
+      child: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
