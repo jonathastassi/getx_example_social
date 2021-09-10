9f4b7613c91a381b7a61c381da606de0eb8cc83a
Jonathas Tassi e Silva <jonathastassi@hotmail.com>
2021-09-10 15:14:44 -0300

    fix

--- lib/presentation/pages/user_detail/user_detail_binding.dart
+++ lib/presentation/pages/user_detail/user_detail_binding.dart
@@ -1,7 +1,10 @@
 import 'package:get/get.dart';
+import 'package:getx_example_social/data/repositories/comment_repository.dart';
 import 'package:getx_example_social/data/repositories/post_repository.dart';
+import 'package:getx_example_social/domain/usecases/comment/get_comment_list_by_post.dart';
 import 'package:getx_example_social/domain/usecases/post/get_post_list_by_user.dart';
 import 'package:getx_example_social/presentation/pages/user_detail/user_detail_controller.dart';
+import 'package:getx_example_social/presentation/pages/user_detail/widgets/comment_list/comment_list_controller.dart';
 import 'package:getx_example_social/presentation/pages/user_detail/widgets/post_list/post_list_controller.dart';
 
 class UserDetailBinding implements Bindings {
@@ -19,5 +22,17 @@ class UserDetailBinding implements Bindings {
         ),
       );
     });
+    Get.lazyPut<CommentListController>(
+      () {
+        return CommentListController(
+          getCommentListByPost: GetCommentListByPost(
+            commentRepository: CommentRepository(
+              httpClient: Get.find(),
+            ),
+          ),
+        );
+      },
+      fenix: true,
+    );
   }
 }
--- lib/presentation/pages/user_detail/widgets/post_list/post_list_widget.dart
+++ lib/presentation/pages/user_detail/widgets/post_list/post_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_example_social/presentation/pages/user_detail/widgets/post_list/post_list_controller.dart';
+import 'package:getx_example_social/presentation/widgets/loading_widget.dart';
 
 class PostListWidget extends GetWidget<PostListController> {
   final int userId;
@@ -13,52 +14,57 @@ class PostListWidget extends GetWidget<PostListController> {
       init: controller,
       initState: (_) => controller.fetchList(userId),
       builder: (_) => Obx(
-        () => ListView.builder(
-          physics: BouncingScrollPhysics(),
-          itemBuilder: (context, index) {
-            final postEntity = controller.postList[index];
+        () => Stack(
+          children: [
+            ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemBuilder: (context, index) {
+                final postEntity = controller.postList[index];
 
-            return Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  ListTile(
-                    contentPadding: const EdgeInsets.all(8),
-                    title: Padding(
-                      padding: const EdgeInsets.only(
-                        bottom: 8,
-                      ),
-                      child: Text(
-                        postEntity.title,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w600,
+                return Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        contentPadding: const EdgeInsets.all(8),
+                        title: Padding(
+                          padding: const EdgeInsets.only(
+                            bottom: 8,
+                          ),
+                          child: Text(
+                            postEntity.title,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        subtitle: Text(
+                          postEntity.body,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
                       ),
-                    ),
-                    subtitle: Text(
-                      postEntity.body,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          TextButton(
+                            child: const Text('Comments'),
+                            onPressed: () => controller.openComments(postEntity.id),
+                          ),
+                          const SizedBox(width: 8),
+                        ],
                       ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      TextButton(
-                        child: const Text('Comments'),
-                        onPressed: () => controller.openComments(postEntity.id),
-                      ),
-                      const SizedBox(width: 8),
                     ],
                   ),
-                ],
-              ),
-            );
-          },
-          itemCount: controller.postList.length,
+                );
+              },
+              itemCount: controller.postList.length,
+            ),
+            if (controller.isLoading.value) LoadingWidget(),
+          ],
         ),
       ),
     );
